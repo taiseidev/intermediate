24ed34690a43e2691c97a36f74d8b9ba5bf5742d
onishi taisei <taisei.developer@gmail.com>
2024-01-12 10:22:32 +0900

    🚀 feat: ItemのComposableを作成

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -19,27 +19,40 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.affirmations.data.Datasource
+//import com.example.affirmations.data.Datasource
 import com.example.affirmations.model.Affirmation
+import com.example.affirmations.model.Topic
 import com.example.affirmations.ui.theme.AffirmationsTheme
 
 class MainActivity : ComponentActivity() {
@@ -50,8 +63,7 @@ class MainActivity : ComponentActivity() {
             AffirmationsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
                     TopicsLayout()
                 }
@@ -62,48 +74,84 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TopicsLayout() {
-
+    TopicItem(topic = Topic(R.string.architecture, 58, R.drawable.architecture))
 }
 
 @Composable
-fun AffirmationsApp() {
-    AffirmationList(
-        affirmationList = Datasource().loadAffirmations(),
-    )
-}
-
-@Composable
-fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier) {
-    LazyColumn(modifier = modifier) {
-        items(affirmationList) { affirmation ->
-            AffirmationCard(
-                affirmation = affirmation,
-                modifier = Modifier.padding(8.dp)
-            )
-        }
-    }
-}
-
-@Composable
-fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
-    Card(modifier = modifier) {
-        Column {
+fun TopicItem(topic: Topic, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.wrapContentSize(
+            Alignment.Center
+        )
+    ) {
+        Row(
+            modifier = Modifier.background(Color(0xffe6e1ea), shape = RoundedCornerShape(10))
+        ) {
             Image(
-                painter = painterResource(affirmation.imageResourceId),
-                contentDescription = stringResource(affirmation.stringResourceId),
+                painter = painterResource(id = topic.imageResource),
+                contentDescription = stringResource(id = topic.title),
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(194.dp),
-                contentScale = ContentScale.Crop
+                    .size(68.dp)
+                    .clip(RoundedCornerShape(topStart = 10.dp, bottomStart = 10.dp))
             )
-            Text(
-                text = LocalContext.current.getString(affirmation.stringResourceId),
-                modifier = Modifier.padding(16.dp),
-                style = MaterialTheme.typography.headlineSmall
-            )
+            Column(
+                modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp),
+            ) {
+                Text(text = stringResource(id = topic.title))
+                Spacer(modifier = Modifier.height(4.dp))
+                Row {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_grain),
+                        contentDescription = stringResource(
+                            id = topic.title
+                        )
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(text = topic.footPrints.toString())
+                }
+            }
         }
     }
 }
+//
+//@Composable
+//fun AffirmationsApp() {
+//    AffirmationList(
+//        affirmationList = Datasource().loadAffirmations(),
+//    )
+//}
+//
+//@Composable
+//fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier) {
+//    LazyColumn(modifier = modifier) {
+//        items(affirmationList) { affirmation ->
+//            AffirmationCard(
+//                affirmation = affirmation, modifier = Modifier.padding(8.dp)
+//            )
+//        }
+//    }
+//}
+//
+//@Composable
+//fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
+//    Card(modifier = modifier) {
+//        Column {
+//            Image(
+//                painter = painterResource(affirmation.imageResourceId),
+//                contentDescription = stringResource(affirmation.stringResourceId),
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .height(194.dp),
+//                contentScale = ContentScale.Crop
+//            )
+//            Text(
+//                text = LocalContext.current.getString(affirmation.stringResourceId),
+//                modifier = Modifier.padding(16.dp),
+//                style = MaterialTheme.typography.headlineSmall
+//            )
+//        }
+//    }
+//}
 
 @Preview
 @Composable
