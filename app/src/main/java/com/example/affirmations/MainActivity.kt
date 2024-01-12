23059b7ea9adfc3c9501141c3de449a516b1ab45
onishi taisei <taisei.developer@gmail.com>
2024-01-12 10:47:39 +0900

    🚀 feat: GridViewを作成

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -20,8 +20,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -32,6 +34,9 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -50,6 +55,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.affirmations.data.DataSource
 //import com.example.affirmations.data.Datasource
 import com.example.affirmations.model.Affirmation
 import com.example.affirmations.model.Topic
@@ -65,7 +72,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    TopicsLayout()
+                    TopicsLayout(topics = DataSource.topics)
                 }
             }
         }
@@ -73,20 +80,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TopicsLayout() {
-    TopicItem(topic = Topic(R.string.architecture, 58, R.drawable.architecture))
+fun TopicsLayout(topics: List<Topic>, modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(count = 2),
+        contentPadding = PaddingValues(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(topics) { topic ->
+            TopicItem(topic = topic)
+        }
+    }
 }
 
 @Composable
 fun TopicItem(topic: Topic, modifier: Modifier = Modifier) {
     Box(
-        modifier = modifier.wrapContentSize(
-            Alignment.Center
-        )
+        modifier = modifier
+            .background(Color(0xffe6e1ea), shape = RoundedCornerShape(10))
+            .fillMaxWidth()
     ) {
-        Row(
-            modifier = Modifier.background(Color(0xffe6e1ea), shape = RoundedCornerShape(10))
-        ) {
+        Row {
             Image(
                 painter = painterResource(id = topic.imageResource),
                 contentDescription = stringResource(id = topic.title),
@@ -97,7 +111,7 @@ fun TopicItem(topic: Topic, modifier: Modifier = Modifier) {
             Column(
                 modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp),
             ) {
-                Text(text = stringResource(id = topic.title))
+                Text(text = stringResource(id = topic.title), fontSize = 13.sp)
                 Spacer(modifier = Modifier.height(4.dp))
                 Row {
                     Icon(
@@ -156,5 +170,4 @@ fun TopicItem(topic: Topic, modifier: Modifier = Modifier) {
 @Preview
 @Composable
 private fun TopicsLayoutPreview() {
-    TopicsLayout()
 }
